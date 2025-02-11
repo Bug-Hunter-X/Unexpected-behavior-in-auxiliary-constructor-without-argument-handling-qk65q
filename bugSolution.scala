46926b5415f53927cda3cc8ee41a64e596d3b293
BugHunter-X <askzenai@gmail.com>
2025-02-11 01:10:05 +0100

    Add bugSolution.scala file

---
+++ bugSolution.scala
@@ -0,0 +1,11 @@
+```scala
+class MyClass(val value: Int) {
+  def this() = this(0) // Explicitly provide a default value of 0
+}
+
+val instance1 = new MyClass(5)
+val instance2 = new MyClass()
+
+println(instance1.value) // Output: 5
+println(instance2.value) // Output: 0
+```
